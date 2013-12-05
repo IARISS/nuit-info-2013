efc6446af41494396c149beff623d556b815ea08
Remy <plespagnol@sfr.fr>
2013-12-05 20:29:57 +0100

    commit

--- pages/search.php
+++ pages/search.php
@@ -1,11 +1,45 @@
-<form onSubmit="doSearch(document.getElementById('s').value);return false;">
-		<input type="text" id="s" x-webkit-speech onwebkitspeechchange="doSearch(this.value);" />
-		<input type="submit" class="btn btn-primary" />
-</form>
+<div class="container">
+	
+	<div class="row">
+        <div class="box">
+          <div class="col-lg-12">
+            <hr>
+            <h2 class="intro-text text-center">Rechercher un porduit avec la <strong>reconnaissance vocale</strong></h2>
+            <hr>
+            <form class="text-center col-md-4 col-md-offset-4" onSubmit="doSearch(document.getElementById('s').value);return false;">
+				<input type="text" id="s" class="form-control" x-webkit-speech onwebkitspeechchange="doSearch(this.value);" />
+				<input type="submit" class="btn3d btn btn-primary btn-lg btn-block" />
+				<input id="mic" class="micro" style="width: 15px;border: 0px; background-color: transparent;" onwebkitspeechchange="transcribe(this.value)" x-webkit-speech="" lang="fr" type="text">
+			</form>
+			<div class="clearfix"></div>
+          </div>
+        </div>
+      </div>
+      
 
+</div>
 
 <script type="text/javascript" src="js/jquery-1.10.2.js"></script>
 <script> 
+
+var isCtrl = false;
+$(document).keyup(function (e) {
+
+if(e.which == 17) isCtrl=false;
+
+}).keydown(function (e) {
+
+    if(e.which == 17) isCtrl=true;
+
+    if(e.which == 83 && isCtrl == true) {
+
+        alert("coucou");
+
+   return false;
+
+ }
+
+});
 
 var withConfirmation = false;
 function doSearch(value) {
