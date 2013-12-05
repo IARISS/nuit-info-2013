16fdbe6d2a944e8319a96e4d6a644598b32a6aaf
Remy <plespagnol@sfr.fr>
2013-12-06 00:51:07 +0100

    cc

--- pages/search_result.php
+++ pages/search_result.php
@@ -5,7 +5,9 @@ require('lib/semantics3/Semantics3.php');
  
 $requestor = new Semantics3_Products(SEM3_KEY,SEM3_SECRET);
 
-$html='<div class="container">';
+$html='<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-6 col-md-6">';
 
 //if(isset($_POST['search']))
 //{
@@ -27,35 +29,31 @@ $html='<div class="container">';
 		$url=$product->sitedetails[0]->url;
 		$features=$product->features;
 
-		$html.='<div class="col-md-6 col-md-offset-1" style="height:180px;">
-                <h2>'.$name.'</h2>
-                <h4>'.$category.'</h4>
-                <div class="row">
-                        Prix:<span id="prix">'.$price.'</span><span id="devise">'.$currency.'</span>
-                </div>
-                <div class="row">
-                        Marque:<span id="marque">'.$brand.'</span>
-                </div>
-                <div class="row">
-                        <a href="'.$url.'" class="link-product"><span class="glyphicon glyphicon-shopping-cart"></span>Lien d\'achat</a>
-                </div>
-
-        </div>
-        <div class="row">
-                <div class="row"></div>
-                <div class="col-md-12">
-                        <h1>Caracteristiques</h1>
-                        <ul class="features">';
+		$html.='<div class="col-xs-12 col-sm-6 col-md-6">
+                <div class="well well-sm">
+                <div class="col-sm-6 col-md-4">
+                <img src="http://placehold.it/380x500" alt="" class="img-rounded img-responsive" />
+                    </div>
+                    <div class="col-sm-6 col-md-8">
+                        <h4>
+                           '.$name.'</h4>
+                        <small>'.$category.'<i class="glyphicon glyphicon-map-marker">
+                        </i></small>
+                        <p>
+                            <i class="glyphicon glyphicon-envelope"></i>'.$price.' '.$currency.'
+                            <br />
+                            <i class="glyphicon glyphicon-globe"></i><a href="'.$url.'">Lien</a>
+                            <br />
+                            <i class="glyphicon glyphicon-gift"></i>Marque : '.$brand.'/p>
+                            <ul>';
 		foreach($features as $key => $feature)
 		{
 			$html.= '<li>'.$key.' : '.$feature.'</li>';
 		}
-		$html .= '</ul>
-                </div>
-        </div>';
+		$html .= '</ul></div>';
 	}
 //}
-$html .= '</div>';
+$html .= '</div></div></div></div>';
 
 echo $html;
 ?>
