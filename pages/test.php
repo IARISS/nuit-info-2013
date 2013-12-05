834713faa3e593887d21b01308d3987156f86c80
Antique Dernnell <a.dernnell@gmail.com>
2013-12-05 20:36:37 +0100

    modif banale

--- pages/test.php
+++ pages/test.php
@@ -1,5 +1,16 @@
 <?php
 
+require('include/define.php');
+require('lib/semantics3/Semantics3.php');
+ 
+$requestor = new Semantics3_Products(SEM3_KEY,SEM3_SECRET);
+ 
+$requestor->products_field("cat_id", 4992);
+$requestor->products_field("brand", "Toshiba");
+ 
+$results = $requestor->get_products();
+ 
 echo 'Page de test';
+echo $results;
 
 ?>
