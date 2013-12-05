a384805c0e4cbeb9ebdca65212f1151216226ce2
Antique Dernnell <a.dernnell@gmail.com>
2013-12-05 21:36:31 +0100

    script php recherche de produit

---
+++ pages/search_result.php
@@ -0,0 +1,24 @@
+<?php
+
+require('include/define.php');
+require('lib/semantics3/Semantics3.php');
+ 
+$requestor = new Semantics3_Products(SEM3_KEY,SEM3_SECRET);
+
+if(isset($_POST['search']))
+{
+	$search=$_POST['search'];
+	$requestor->products_field("search", $search);
+	$results = $requestor->get_products();
+	$dr = json_decode($results);
+	$count=$dr['results_count'];
+	$tableau=$dr['results'];
+	foreach ($tableau as $product)
+	{
+		$name=$product['name'];
+		echo $name;
+		echo '\n';
+	}
+}
+
+?>
--- pages/test.php
+++ pages/test.php
@@ -5,12 +5,12 @@ require('lib/semantics3/Semantics3.php');
  
 $requestor = new Semantics3_Products(SEM3_KEY,SEM3_SECRET);
  
-$requestor->products_field("cat_id", 4992);
-$requestor->products_field("brand", "Toshiba");
+$requestor->products_field("search", "pantalon rouge");
+
  
 $results = $requestor->get_products();
  
 echo 'Page de test';
-echo $results;
+var_dump(json_decode($results));
 
 ?>
