ac3c49eabc19e351485f918757f66194a08c15dd
Remy <plespagnol@sfr.fr>
2013-12-05 20:57:57 +0100

    commit

--- pages/search.php
+++ pages/search.php
@@ -9,8 +9,9 @@
             <form class="text-center col-md-4 col-md-offset-4" onSubmit="doSearch(document.getElementById('s').value);return false;">
 				<input type="text" id="s" class="form-control" x-webkit-speech onwebkitspeechchange="doSearch(this.value);" />
 				<input type="submit" class="btn3d btn btn-primary btn-lg btn-block" />
-				<input id="mic" class="micro" style="width: 15px;border: 0px; background-color: transparent;" onwebkitspeechchange="transcribe(this.value)" x-webkit-speech="" lang="fr" type="text">
 			</form>
+			<div style="margin-top:20px;" id="message"></div>
+			
 			<div class="clearfix"></div>
           </div>
         </div>
@@ -21,25 +22,6 @@
 
 <script type="text/javascript" src="js/jquery-1.10.2.js"></script>
 <script> 
-
-var isCtrl = false;
-$(document).keyup(function (e) {
-
-if(e.which == 17) isCtrl=false;
-
-}).keydown(function (e) {
-
-    if(e.which == 17) isCtrl=true;
-
-    if(e.which == 83 && isCtrl == true) {
-
-        alert("coucou");
-
-   return false;
-
- }
-
-});
 
 var withConfirmation = false;
 function doSearch(value) {
@@ -95,7 +77,7 @@ function doSearch(value) {
 		}
 	}
 	else{
-		$('#message').addClass('alert alert-error').html('<span class="label label-important">Erreur</span> Aucune action associée à la saisie, désolé...');
+		$('#message').addClass('alert alert-danger col-md-6 col-md-offset-3').html('<span class="label label-danger">Erreur</span> Aucune action associée à la saisie, désolé...');
 	}
 }
 
