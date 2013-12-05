8b0bee6850ac79000603eb5ad604e775318f389f
tuxtux59 <thibaud.renaux@gmail.com>
2013-12-06 00:42:41 +0100

    modifs
    by tux

---
+++ pages/profiltype.php
@@ -0,0 +1,22 @@
+<div class="container">
+	<h2>Profils types</h2>
+	<a href=""><div class="col-md-2" style="border:1px solid white;height:180px;">
+
+	</div></a>
+	<div class="col-md-2 col-md-offset-1" style="border:1px solid white;height:180px;">
+		<h4>Informaticien</h4>
+	</div>
+	<div class="col-md-2 col-md-offset-1" style="border:1px solid white;height:180px;">
+
+	</div>
+	<div class="col-md-2 col-md-offset-1" style="border:1px solid white;height:180px;">
+
+	</div>
+	<div class="col-md-2" style="border:1px solid white;height:180px;margin-top:25px;">
+
+	</div>
+	<div class="col-md-2 col-md-offset-1" style="border:1px solid white;height:180px;margin-top:25px;" hidden="true">
+
+	</div>
+
+</div>
