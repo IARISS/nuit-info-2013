154f7655e75979c4cd832ab01ad930fb25b0352d
tuxtux59 <thibaud.renaux@gmail.com>
2013-12-06 01:16:09 +0100

    profil type proposées
    création des profils types

--- pages/profiltype.php
+++ pages/profiltype.php
@@ -1,21 +1,80 @@
 <div class="container">
 	<h2>Profils types</h2>
-	<a href=""><div class="col-md-2" style="border:1px solid white;height:180px;">
+	<div style="text-align:center;">
+		<a href="" onclick=""><div class="col-md-2" style="border:1px solid white;height:180px;">
+			<h4>Homme</h4>
+		</div></a>
+		<div class="col-md-2 col-md-offset-1" style="border:1px solid white;height:180px;">
+			<h4>Femme</h4>
+		</div>
+		<div class="col-md-2 col-md-offset-1" style="border:1px solid white;height:180px;">
+			<h4>15-25</h4>
+		</div>
+		<div class="col-md-2 col-md-offset-1" style="border:1px solid white;height:180px;">
+			<h4>25-40</h4>
+		</div>
+		<div class="col-md-2" style="border:1px solid white;height:180px;margin-top:25px;">
+			<h4>40-60</h4>
+		</div>
+		<div class="col-md-2 col-md-offset-1" style="border:1px solid white;height:180px;margin-top:25px;" >
+			<h4>60 et +</h4>
 
-	</div></a>
-	<div class="col-md-2 col-md-offset-1" style="border:1px solid white;height:180px;">
-		<h4>Informaticien</h4>
-	</div>
-	<div class="col-md-2 col-md-offset-1" style="border:1px solid white;height:180px;">
+		</div>
+		<div class="col-md-2 col-md-offset-1" style="border:1px solid white;height:180px;margin-top:25px;" >
+			<h4>Globe-trotter</h4>
 
-	</div>
-	<div class="col-md-2 col-md-offset-1" style="border:1px solid white;height:180px;">
+		</div>
+		<div class="col-md-2 col-md-offset-1" style="border:1px solid white;height:180px;margin-top:25px;" >
+			<h4>Vie d'intérieur</h4>
 
-	</div>
-	<div class="col-md-2" style="border:1px solid white;height:180px;margin-top:25px;">
+		</div>
+		<div class="col-md-2" style="border:1px solid white;height:180px;margin-top:25px;">
+			<h4>Action</h4>
+		</div>
+		<div class="col-md-2 col-md-offset-1" style="border:1px solid white;height:180px;margin-top:25px;" >
+			<h4>Culture</h4>
 
-	</div>
-	<div class="col-md-2 col-md-offset-1" style="border:1px solid white;height:180px;margin-top:25px;" hidden="true">
+		</div>
+		<div class="col-md-2 col-md-offset-1" style="border:1px solid white;height:180px;margin-top:25px;" >
+			<h4>Détente</h4>
+
+		</div>
+		<div class="col-md-2 col-md-offset-1" style="border:1px solid white;height:180px;margin-top:25px;" >
+			<h4>Informatique</h4>
+
+		</div>
+		<div class="col-md-2 " style="border:1px solid white;height:180px;margin-top:25px;" >
+			<h4>Bricolage</h4>
+
+		</div>
+		<div class="col-md-2 col-md-offset-1" style="border:1px solid white;height:180px;margin-top:25px;" >
+			<h4>Déco</h4>
+
+		</div>
+		<div class="col-md-2 col-md-offset-1" style="border:1px solid white;height:180px;margin-top:25px;" >
+			<h4>Cuisine</h4>
+
+		</div>
+		<div class="col-md-2 col-md-offset-1" style="border:1px solid white;height:180px;margin-top:25px;" >
+			<h4>Automobile</h4>
+
+		</div>
+		<div class="col-md-2 " style="border:1px solid white;height:180px;margin-top:25px;" >
+			<h4>Habillement</h4>
+
+		</div>
+		<div class="col-md-2 col-md-offset-1" style="border:1px solid white;height:180px;margin-top:25px;" >
+			<h4>Voyage</h4>
+
+		</div>
+		<div class="col-md-2 col-md-offset-1" style="border:1px solid white;height:180px;margin-top:25px;" >
+			<h4>Bien-être</h4>
+
+		</div>
+		<div class="col-md-2 col-md-offset-1" style="border:1px solid white;height:180px;margin-top:25px;" >
+			<h4>Jardin</h4>
+
+		</div>
 
 	</div>
 
