f9c93cd630ce3d00540fb32ae1de4734ca6c2e84
Remy <plespagnol@sfr.fr>
2013-12-06 02:35:32 +0100

    d

--- pages/searchPage.php
+++ pages/searchPage.php
@@ -22,7 +22,6 @@
 		e.preventDefault();
 		var search = $('#search').val();
 		$.post('ajax/search_result.php',{search:search}, function(data){
-			alert(data);
 			$('.result').html(data);
 		})
 	})
