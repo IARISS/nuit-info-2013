282b269682344b32fde6ccdb2063cf2ec62a1b02
Antique Dernnell <a.dernnell@gmail.com>
2013-12-06 02:17:53 +0100

    r

--- ajax/search_result.php
+++ ajax/search_result.php
@@ -1,7 +1,7 @@
 <?php
 
-require('include/define.php');
-require('lib/semantics3/Semantics3.php');
+require('../include/define.php');
+require('../lib/semantics3/Semantics3.php');
  
 $requestor = new Semantics3_Products(SEM3_KEY,SEM3_SECRET);
 
