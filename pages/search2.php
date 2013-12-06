07253a81b8f1c01a393e340eb6f69f577bca677a
Antique Dernnell <a.dernnell@gmail.com>
2013-12-06 03:37:59 +0100

    t

--- pages/search2.php
+++ pages/search2.php
@@ -6,7 +6,8 @@
             <h2 class="intro-text text-center">Rechercher un produit</h2>
             <hr>
             <form class="text-center col-md-4 col-md-offset-4">
-				<input id="search"	class="form-control" type="text"/>
+				<input id="category"	class="form-control" type="text"/>
+				<input id="search" class="form-control" type="text"/>
 				<button class="btn3d btn btn-primary btn-lg btn-block" id="actionSearch">Rechercher</button>
 			</form>	
 			<div class="result"></div>		
@@ -21,9 +22,12 @@
 	$('#actionSearch').click(function(e){
 		e.preventDefault();
 		var search = $('#search').val();
-		$.post('ajax/get_cat_id.php',{category:search}, function(data){
-			alert(data);
-			$('.result').html(data);
+		var category = $('#category').val();
+		$.post('ajax/get_cat_id.php',{category:category}, function(data){
+			var id = data.id;
+			$post('ajax/search_result.php',{search:search,cat_id:id},function(data){
+				$('.result').html(data);
+			})
 		})
 	})
 </script>
