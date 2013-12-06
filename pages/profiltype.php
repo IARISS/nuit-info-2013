88735f21d6a04846a51ae6ecc268c002c9cfd606
tuxtux59 <thibaud.renaux@gmail.com>
2013-12-06 02:59:24 +0100

    commit this
    lol

--- pages/profiltype.php
+++ pages/profiltype.php
@@ -1,27 +1,49 @@
+<script src="https://www.google.com/jsapi"></script>
+<script >
+	
+		function secondstep(){
+			
+            document.getElementById("age1").style.display = 'block';
+            document.getElementById("age2").style.display = 'block';
+            document.getElementById("age3").style.display = 'block';
+            document.getElementById("age4").style.display = 'block';
+
+		}
+		function thirddstep(){
+			
+            document.getElementById("age1").style.display = 'block';
+            document.getElementById("age2").style.display = 'block';
+            document.getElementById("age3").style.display = 'block';
+            document.getElementById("age4").style.display = 'block';
+              
+		}
+	
+</script>
 <div class="container">
 	<h2>Profils types</h2>
-	<div style="text-align:center;">
-		<a href="" onclick=""><div class="col-md-2" style="border:1px solid white;height:180px;">
-			<h4>Homme</h4>
+	<div style="text-align:center;color:blue;vertical-align:bottom;">
+		 <div class="col-md-2" name="sex" onclick="secondstep();" style="border:1px solid white;height:180px;background-image:url('vincitux.png');">
+		 	
+			<h4 style="width:100%;background-color:#ddd;color:gray;">Homme</h4>
 		</div></a>
-		<div class="col-md-2 col-md-offset-1" style="border:1px solid white;height:180px;">
-			<h4>Femme</h4>
+		<div class="col-md-2 col-md-offset-1" name="sex" id="sex" style="border:1px solid white;height:180px;background-image:url('female.png');">
+			<h4 style="width:100%;background-color:#ddd;color:gray;">Femme</h4>
 		</div>
-		<div class="col-md-2 col-md-offset-1" style="border:1px solid white;height:180px;">
+		<div class="col-md-2 col-md-offset-1" name="age1" id="age1" style="border:1px solid white;height:180px;display:none;">
 			<h4>15-25</h4>
 		</div>
-		<div class="col-md-2 col-md-offset-1" style="border:1px solid white;height:180px;">
+		<div class="col-md-2 col-md-offset-1" name="age2" id="age2" style="border:1px solid white;height:180px;display:none;">
 			<h4>25-40</h4>
 		</div>
-		<div class="col-md-2" style="border:1px solid white;height:180px;margin-top:25px;">
+		<div class="col-md-2" name="age" id="age3" style="border:1px solid white;height:180px;margin-top:25px;display:none;">
 			<h4>40-60</h4>
 		</div>
-		<div class="col-md-2 col-md-offset-1" style="border:1px solid white;height:180px;margin-top:25px;" >
+		<div class="col-md-2 col-md-offset-1" name="age4" id="age4" style="border:1px solid white;height:180px;margin-top:25px;display:none;" >
 			<h4>60 et +</h4>
 
 		</div>
 		<div class="col-md-2 col-md-offset-1" style="border:1px solid white;height:180px;margin-top:25px;" >
-			<h4>Globe-trotter</h4>
+			<h4 style="width:100%;background-color:#ddd;color:gray;">Globe-trotter</h4>
 
 		</div>
 		<div class="col-md-2 col-md-offset-1" style="border:1px solid white;height:180px;margin-top:25px;" >
