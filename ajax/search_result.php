03cfa616ee5a8fdc2f32b36269075f975ef855c8
Antique Dernnell <a.dernnell@gmail.com>
2013-12-06 06:44:31 +0100

    u

--- ajax/search_result.php
+++ ajax/search_result.php
@@ -83,7 +83,7 @@ if(isset($_POST['search']) && isset($_POST['cat_id']))
             <div class="well">
                 <div class="row">
                     <div class="col-sm-6 col-md-4">
-                        <img src="'.var_dump($res).'" alt="" class="img-rounded img-responsive" />
+                        <img src="'.'#'.'" alt="" class="img-rounded img-responsive" />
                     </div>
                     <div class="col-sm-6 col-md-8">
                         <h4>'.$name.'</h4>
