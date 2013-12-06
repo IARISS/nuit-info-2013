693f7a134af460d334165ace05fd993c09636fde
Antique Dernnell <a.dernnell@gmail.com>
2013-12-06 03:22:00 +0100

    y

--- ajax/get_cat_id.php
+++ ajax/get_cat_id.php
@@ -7,7 +7,15 @@ $requestor = new Semantics3_Products(SEM3_KEY,SEM3_SECRET);
 
 if(isset($_POST['category']))
 {
-	
+	$requestor->categories_field("name", $_POST['category']);
+	$results = $requestor->get_categories();
+	$dr = json_decode($results);
+
+	$count=$dr->{'results_count'};
+	$tableau=$dr->{'results'};
+
+	$id=$tableau[0]->cat_id;
+	return json_encode({"cat_id",$id});
 }
 
 ?>
