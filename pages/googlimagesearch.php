a664db44d7467f5e296f11cc85a17f4f0d38e141
tuxtux59 <thibaud.renaux@gmail.com>
2013-12-06 05:53:10 +0100

    ggiùg
    ggimg

--- pages/googlimagesearch.php
+++ pages/googlimagesearch.php
@@ -1,21 +1,4 @@
-<div class="row">
-                    <div class="col-sm-6 col-md-4">
-                        <img src="http://placehold.it/380x500" alt="" id ="img" class="img-rounded img-responsive">
-                    </div>
-                    <div class="col-sm-6 col-md-8">
-                        <h4>Apple Mac Mini MD387LL/A Desktop</h4>
-                        Catégorie : Desktops
-                        <p>
-                            Prix : 588.00 USD
-                            <br>
-                            Marque : Apple
-                            <br>
-              <a href="http://www.frys.com/product/7378624"><small>http://www.frys.com/product/7378624</small></a>
-              <br>
-              Propriétés : </p><ul><li>Memory Speed : 1600 MHz</li><li>Capacity : 1 x 500 GB</li><li>Device Type : Power supply</li><li>Nominal Voltage : AC 120/230 V ( 50/60 Hz )</li><li>ENERGY STAR Qualified : Yes</li><li>64-bit Computing : Yes</li><li>Power Consumption Operational : 85 Watt</li><li>Technology : DDR3 SDRAM</li><li>Max Monitors Supported : 2</li><li>Max Operating Temperature : 95 Â°F</li><li>Sound Emission : 12 dBA</li><li>Service &amp; Support : 1 year warranty</li><li>Service &amp; Support Details : Limited warranty - 1 year Technical support - phone consulting - 90 days</li><li>Localization : English / United States</li><li>Graphics Processor : Intel HD Graphics 4000 Dynamic Video Memory Technology 5.0</li><li>Monitor Type : None.</li><li>Min Operating Temperature : 50 Â°F</li><li>Cables Included : HDMI-DVI adapter</li><li>Interface Type : SATA-300</li><li>Sound Output Mode : Stereo</li><li>Supported Flash Memory Cards : SD Memory Card, SDHC Memory Card, SDXC Memory Card</li><li>OS Provided : Apple OS X 10.8 Mountain Lion - English</li><li>Max CPU Qty : 1</li><li>Humidity Range Operating : 5 - 95% (non-condensing)</li><li>Interfaces : 1 x Thunderbolt 1 x HDMI 4 x USB 3.0 1 x FireWire 800 1 x LAN (Gigabit Ethernet) 1 x SPDIF input/line-in/microphone 1 x SPDIF output/headphones/line-out 1 x IrDA</li><li>Wireless LAN Supported : Yes</li><li>EPEAT Compliant : EPEAT Gold</li><li>Controller Interface Type : SATA-300</li><li>Spindle Speed : 5400 rpm</li><li>Max Turbo Speed : 3.1 GHz</li><li>Other Features : Aluminum enclosure</li><li>Processor Main Features : Intel Turbo Boost Technology 2</li><li>Compliant Standards : IEEE 802.11b, IEEE 802.11a, IEEE 802.11g, IEEE 802.11n, Bluetooth 4.0</li><li>Cache Per Processor : 3 MB</li><li>Video Interfaces : HDMI, DisplayPort</li><li>Installed Size : 4 GB / 16 GB (max)</li><li>Product Form Factor : Desktop slimline</li><li>Data Link Protocol : Ethernet, Fast Ethernet, Gigabit Ethernet, IEEE 802.11b, IEEE 802.11a, IEEE 802.11g, IEEE 802.11n, Bluetooth 4.0</li><li>Built-in Devices : Speaker, antenna, infrared receiver</li><li>CPU : Intel Core i5 (3rd Gen) 2.5 GHz</li><li>CPU Qty : 1</li><li>Number of Cores : Dual-Core</li><li>Slots : 2 (total) / 0 (free) x memory</li><li>Speakers : 1 x mixed channel</li><li>Configuration Features : 2 x 2 GB</li><li>Software : Apple iTunes, Apple Mac OS X Mail, Apple iLife, Apple Safari, Photo Booth, Apple Time Machine, Apple FaceTime, Reminders, App Store, Contacts, Calendar, Notification Center, Messages, Dictation, iCloud, Game Center</li>              </ul>
-            <p></p>
-          </div>
-                </div>
+
     <script src="https://www.google.com/jsapi"></script>
     <script >
 
@@ -27,9 +10,7 @@
         // Check that we got results
         if (imageSearch.results && imageSearch.results.length > 0) {
 
-         
-
-          // Loop through our results, printing them to the page.
+          
           var results = imageSearch.results;
           
             var result = results[0];
@@ -39,31 +20,23 @@
             newImg.alt = result.titleNoFormatting;
             newImg.title = newImg.alt;
             imgContainer.appendChild(newImg);
-
-            // Put our title + image in the content
            contentDiv.appendChild(imgContainer);
-          
-
-          // Now add links to additional pages of search results.
           
         }
       }
 
       function OnLoad(hdhdth) {
       
-        // Create an Image Search instance.
+      
         imageSearch = new google.search.ImageSearch();
 
-        // Set searchComplete as the callback function when a search is 
-        // complete.  The imageSearch object will have results in it.
         imageSearch.setSearchCompleteCallback(this, searchComplete, null);
 
-        // Find me a beautiful car.
+        
         <?php 
         $name = 'iphone 5s';
         echo 'imageSearch.execute("'.$name.'");';
         ?>
-        // Include the required Google branding
         google.search.Search.getBranding('branding');
       }
      google.setOnLoadCallback(OnLoad);
