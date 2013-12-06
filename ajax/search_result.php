4e1f7e3972b39640c695855ae3e1a062d6f95796
Remy <plespagnol@sfr.fr>
2013-12-06 05:56:12 +0100

    sdf

--- ajax/search_result.php
+++ ajax/search_result.php
@@ -77,7 +77,7 @@ if(isset($_POST['search']) && isset($_POST['cat_id']))
             <div class="well">
                 <div class="row">
                     <div class="col-sm-6 col-md-4">
-                        <img src="'.$res[0].'" alt="" class="img-rounded img-responsive" />
+                        <img src="" alt="" class="img-rounded img-responsive" />
                     </div>
                     <div class="col-sm-6 col-md-8">
                         <h4>'.$name.'</h4>
