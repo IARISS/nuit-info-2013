c942935f4ea154f999730d4b07778389cea54f18
Remy <plespagnol@sfr.fr>
2013-12-06 04:44:03 +0100

    Index

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 //Template maitre, les pages supplémentaires sont à mettre dans le dossier pages
 if (empty($_GET['page'])) {
-    header('Location: /home');
+    header('Location: /searchPage');
     exit();
 }
 
